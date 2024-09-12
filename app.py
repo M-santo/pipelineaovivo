1636f197bb6f7983d279e3522c371001e1b4ee98
MS-Marcelo <marcelo_santo1@outlook.com>
2024-09-12 10:29:20 -0300

    feat: adiconar sqlalchemy

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
-import contrato
+from  contrato import Vendas
 from datetime import datetime, time
+from pydantic import BaseModel, ValidationError
 import re
 
 # Função para validar o formato do email
@@ -18,17 +19,24 @@ def main():
     hora = st.time_input("Hora da compra", value=time(9, 0) ) # Valor padrão: 09:00
     valor = st.number_input("Valor da venda", min_value=0.0, format="%.2f")
     quantidade = st.number_input("Quantidade de produtos", min_value=1, step=1)
-    produto = st.selectbox("Produto", options=["ZpFlow com Gemini", "ZapFlow com chatGPT",])
+    produto = st.selectbox("Produto", options=["ZapFlow com Gemini", "ZapFlow com chatGPT", "ZapFlow com Llama3.0"])
 
     if st.button("Salvar"):
 
-        data_hora = datetime.combine(data, hora)
-        st.write("**Dados da Venda:**")
-        st.write(f"Email do vendedor: {email}")
-        st.write(f"Data e Hora da Compra: {data_hora}")
-        st.write(f"Valor da Venda: R$ {valor:.2f}")
-        st.write(f"Quantidade de Produtos: {quantidade}")
-        st.write(f"Produto: {produto}")
+        try:
+            data_hora = datetime.combine(data, hora)
+
+            venda = Vendas(
+                email= email,
+                data = data_hora,
+                valor = valor,
+                quantidade = quantidade,
+                produto = produto
+            )
+
+            st.write(venda)
+        except ValidationError as e:
+            st.error(f"Deu erro {e}")
 
 if __name__=="__main__":
     main()
